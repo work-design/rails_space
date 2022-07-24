fe22853aed6ef37d73b2ff26c6c3a8f5f94af940
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-24 21:06:19 +0800

    route

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,41 @@
 Rails.application.routes.draw do
-
-  namespace :space, defaults: { business: 'space' } do
-    resources :grids, only: [] do
-      member do
-        get :qrcode
-      end
-    end
-    resources :rooms, only: [] do
-      member do
-        get :qrcode
-      end
-    end
-    namespace :admin, defaults: { namespace: 'admin' } do
-      root 'home#index'
-      resources :stations do
-        resources :buildings
-        resources :rooms do
-          resources :grids
-        end
-      end
-    end
-    namespace :me, defaults: { namespace: 'me' } do
-      resources :rooms do
+  scope RailsCom.default_routes_scope do
+    namespace :space, defaults: { business: 'space' } do
+      resources :grids, only: [] do
         member do
-          post :in
-          post :out
           get :qrcode
         end
-        resources :grids do
+      end
+      resources :rooms, only: [] do
+        member do
+          get :qrcode
+        end
+      end
+      namespace :admin, defaults: { namespace: 'admin' } do
+        root 'home#index'
+        resources :stations do
+          resources :buildings
+          resources :rooms do
+            resources :grids
+          end
+        end
+      end
+      namespace :me, defaults: { namespace: 'me' } do
+        resources :rooms do
           member do
             post :in
             post :out
             get :qrcode
           end
+          resources :grids do
+            member do
+              post :in
+              post :out
+              get :qrcode
+            end
+          end
         end
       end
     end
   end
-
 end
