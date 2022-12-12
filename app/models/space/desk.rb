c205904790a62258cf332aae8c4d71833ebba8c1
qinmingyuan <mingyuan0715@foxmail.com>
2022-12-12 18:57:31 +0800

    desk logic

--- app/models/space/desk.rb
+++ app/models/space/desk.rb
@@ -1,5 +1,20 @@
 module Space
   class Desk < ApplicationRecord
     include Model::Desk
+    include JiaBo::Ext::Device
+
+    def print
+      r = room.station.organ.device.print(
+        data: to_tspl
+      )
+    end
+
+    def to_tspl
+      ts = BaseTspl.new
+      ts.qrcode(qrcode_product_url)
+      ts.text(name, x: 20, y: 15, x_scale: 2, y_scale: 2)
+      ts.render
+    end
+
   end
 end
