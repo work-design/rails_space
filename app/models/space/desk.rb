1420f180cb8ac5c376ea69e5d437a27236d187da
qinmingyuan <mingyuan0715@foxmail.com>
2022-12-17 18:49:45 +0800

    gem

--- app/models/space/desk.rb
+++ app/models/space/desk.rb
@@ -13,7 +13,7 @@ module Space
       ts = BaseTspl.new
       ts.qrcode(product_url, x: 20, y: 10, cell_width: 10)
       ts.text(name, x: 320, y: 15, x_scale: 2, y_scale: 2)
-      ts.text('扫码点餐', x:320, y: 40, x_scale: 2, y_scale: 2)
+      ts.text('扫码点餐', x:320, y: 40)
       ts.render
     end
 
