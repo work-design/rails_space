bea4d1807f002a1915e1155a9d81989ae9567161
qinmingyuan <mingyuan0715@foxmail.com>
2024-10-29 00:07:45 +0800

    smart out

--- app/models/space/ext/item.rb
+++ app/models/space/ext/item.rb
@@ -6,17 +6,17 @@ module Space
       belongs_to :desk, class_name: 'Space::Desk', optional: true
       belongs_to :station, class_name: 'Space::Station', optional: true
 
-      after_create_commit :send_notice_to_desk
+      has_many :desk_brothers, ->(o) { where(desk_id: o.desk_id) }, class_name: self.name, primary_key: :organ_id, foreign_key: :organ_id
+
+      after_commit :send_notice_to_desk, on: [:create, :destroy]
     end
 
     def send_notice_to_desk
-      return unless id
       broadcast_action_to(
-        desk,
-        action: :append,
-        target: 'body',
-        partial: 'visit',
-        locals: { model: self }
+        "desk_count_#{organ_id}",
+        action: :update,
+        target: "desk_count_#{desk_id}",
+        content: desk_brothers.todo.count
       )
     end
 
