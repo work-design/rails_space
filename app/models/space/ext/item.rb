5947259c270536824df22ac7e7610276570e2d8f
qinmingyuan <mingyuan0715@foxmail.com>
2024-10-28 23:28:06 +0800

    send notice to desk

--- app/models/space/ext/item.rb
+++ app/models/space/ext/item.rb
@@ -5,6 +5,19 @@ module Space
     included do
       belongs_to :desk, class_name: 'Space::Desk', optional: true
       belongs_to :station, class_name: 'Space::Station', optional: true
+
+      after_create_commit :send_notice_to_desk
+    end
+
+    def send_notice_to_desk
+      return unless id
+      broadcast_action_to(
+        desk,
+        action: :append,
+        target: 'body',
+        partial: 'visit',
+        locals: { model: self }
+      )
     end
 
   end
