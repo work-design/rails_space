51eebd8129c1239c342aec9b8772a2f031b2ecab
qinmingyuan <mingyuan0715@foxmail.com>
2024-12-15 22:01:04 +0800

    optional

--- app/models/space/model/station.rb
+++ app/models/space/model/station.rb
@@ -9,7 +9,7 @@ module Space
       attribute :buildings_count, :integer, default: 0
       attribute :rooms_count, :integer, default: 0
 
-      belongs_to :organ, class_name: 'Org::Organ'
+      belongs_to :organ, class_name: 'Org::Organ', optional: true
       belongs_to :area, class_name: 'Ship::Area'
       has_taxons :area
 
